ac4486f048cb9e19bf320236a53bd9c59274a605
adesokandijam <adesokanmajid@gmail.com>
2023-12-20 17:07:58 +0100

    adding the code for the app and configuring the first pipeline

--- app/app.py
+++ app/app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, jsonify
+import boto3
+import json
 
 app = Flask(__name__)
 
-@app.route('/hello/', methods=['GET', 'POST'])
-def welcome():
-    return "Hello World!"
+def read_s3_json(bucket_name, key):
+    s3 = boto3.client('s3')
+    try:
+        return json.loads(s3.get_object(Bucket=bucket_name, Key=key)['Body'].read().decode('utf-8'))
+    except Exception as e:
+        return f"Error: {e}"
+
+bucket_name, file_key = 'payaza-devops-assessment-test-bucket', 'assessment.json'
+
+@app.route('/message')
+def display_json():
+    return jsonify(read_s3_json(bucket_name, file_key))
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=8000)
+    app.run(debug=True,port=8000)
